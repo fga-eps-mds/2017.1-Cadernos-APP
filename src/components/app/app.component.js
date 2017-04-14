2b5e8e7057920ee17138542184f6b32633f9066c
Fabio Teixeira <fabio1079@gmail.com>
2017-04-14 12:23:30 -0300

    Add start screen
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

---
+++ src/components/app/app.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import { styles } from './app.style';
+
+import { StartScreen } from '../start-screen';
+
+export class AppComponent extends Component {
+    render() {
+        return (
+            <StartScreen />
+        );
+    }
+}
---
+++ src/components/start-screen/start-screen.style.js
@@ -0,0 +1,33 @@
+export const styles = {
+    mainContent: {
+        padding: 10,
+        flex: 1,
+    },
+
+    topContent: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+
+    bottomContent: {
+        flex: 1,
+    },
+
+    button: {
+        marginBottom: 10
+    },
+
+    text: {
+        textAlign: 'center'
+    },
+
+    title: {
+        fontSize: 38
+    },
+
+    textBetweenButtons: {
+        marginTop: 10,
+        marginBottom: 20,
+    }
+}
+
