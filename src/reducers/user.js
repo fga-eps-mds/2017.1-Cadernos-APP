adc21bee582dca12c5e3573da1f62b60c9b56f58
Fabio Teixeira <fabio1079@gmail.com>
2017-04-17 15:58:27 -0300

    Use immutability-helper to update state on the reducers
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,3 +1,5 @@
+import update from 'immutability-helper';
+
 import initialState from '../config/initial-state';
 import Actions from '../config/actions';
 
@@ -5,24 +7,30 @@ import Actions from '../config/actions';
 const userReducer = (state=initialState.user, action) => {
   switch(action.type) {
     case Actions.User.SET_USER:
-      return {
-        id: action.id,
-        name: action.name,
-        email: action.email,
-        auth_token: action.auth_token,
-        errors: action.errors
-      };
+      return update(state, {
+        $set: {
+          id: action.id,
+          name: action.name,
+          email: action.email,
+          auth_token: action.auth_token,
+          errors: action.errors
+        }
+      });
 
 
     case Actions.User.SET_USER_ERRORS:
-      return Object.assign({}, state, {
-        errors: action.errors
+      return update(state, {
+        errors: {
+          $set: action.errors
+        }
       });
 
 
     case Actions.User.TOGGLE_USER_REMEMBER_LOGIN:
-      return Object.assign({}, state, {
-        rememberLogin: !state.rememberLogin
+      return update(state, {
+        rememberLogin: {
+          $set: !state.rememberLogin
+        }
       });
 
 
