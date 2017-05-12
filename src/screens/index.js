03d5f4a76b23af5ff3fced95b8445a659ab51e94
Victor Moura <victor-cmoura@uol.com.br>
2017-05-12 10:53:18 -0300

    Fixed UserLoginScreen importing into screens index

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,6 @@
 import CreateUserScreen from './create-user/create-user.container';
 import MainScreen from './main/main.component';
-import UserLoginScreen from './user-login/user-login.component';
+import UserLoginScreen from './user-login/user-login.container';
 
 export {
   CreateUserScreen,
