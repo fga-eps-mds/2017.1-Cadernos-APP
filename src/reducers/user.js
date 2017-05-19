51cd94ce01b27e4c6a9b5ffcc8877e27cb49c936
Victor Moura <victor-cmoura@uol.com.br>
2017-05-19 15:24:31 -0300

    Added reducer and action type for setting a visitor

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,4 +1,4 @@
-import { USER_REGISTER, USER_SET, USER_SENDING_DATA, USER_ERRORS, USER_LOGIN, USER_AUTHENTICATED, USER_LOGOUT, CLEAN_USER_AUTHENTICATION_ERRORS} from '../config/actions-types';
+import { USER_REGISTER, USER_SET, USER_SENDING_DATA, USER_ERRORS, USER_LOGIN, VISITOR_LOGIN, USER_AUTHENTICATED, USER_LOGOUT, CLEAN_USER_AUTHENTICATION_ERRORS} from '../config/actions-types';
 
 import initialState from '../config/initial-state';
 
@@ -47,7 +47,11 @@ const userReducer = (state = initialState.user, action) => {
         ...state,
         errors:{...action.errors}
       }
-
+    case VISITOR_LOGIN:
+      return{
+        ...state,
+        isVisitor: action.isVisitor
+      }
     default:
       return state;
   }
