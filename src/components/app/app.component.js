aba8ab7437141e15a36fbc8dd7c2c4ac09f56537
Lucas <lucas50xd@hotmail.com>
2017-05-26 20:59:52 -0300

    setting up drawer

--- src/components/app/app.component.js
+++ src/components/app/app.component.js
@@ -14,20 +14,59 @@ import {
   ProfileScreen
 } from '../../screens';
 
+import { EventEmitter } from 'fbemitter';
+import SideBar from '../../screens/side-bar/side-bar.component'
+import Drawer from 'react-native-drawer';
+import NavigationDrawer from '../app-drawer/app-drawer.component';
+
+let _emitter = new EventEmitter();
+
 export default class App extends Component {
+  componentDidMount() {
+    var self = this;
+
+    _emitter.addListener('openMenu', () => {
+      self._drawer.open();
+    });
+
+    _emitter.addListener('back', () => {
+      self._navigator.pop();
+    });
+  }
+
+
   render() {
     return (
-      <Router hideNavBar={true}>
-        <Scene key="Main" component={MainScreen} initial />
-        <Scene key="CreateUser" component={CreateUserScreen} />
-        <Scene key="UserLogin" component={UserLoginScreen} />
-        <Scene key="Home" component={HomeScreen} />
-        <Scene key="Profile" component={ProfileScreen} />
-        <Scene key="EditUser" component={EditUserScreen} />
-        <Scene key="CreateBook" component={CreateBookScreen} />
-        <Scene key="ViewBook" component={ViewBookScreen} />
-        <Scene key="EditBook" component={EditBookScreen} />
-      </Router>
+      <Drawer
+        ref={(ref) => this._drawer = ref}
+        content={<SideBar />}
+        tapToClose
+        openDrawerOffset={0.2}
+        panOpenMask={0.2}
+        panCloseMask={0.2}
+        negotiatePan
+        tweenHandler={(ratio) => ({
+          main: { opacity: Math.max(0.54, 1 - ratio) },
+        })}
+      >
+        <Router hideNavBar={true}>
+          <Scene key='PreLogin'>
+            <Scene key="Main" component={MainScreen} initial />
+            <Scene key="CreateUser" component={CreateUserScreen} />
+            <Scene key="UserLogin" component={UserLoginScreen} />
+
+
+
+            <Scene key="Home" component={HomeScreen} />
+            <Scene key="Profile" component={ProfileScreen} />
+            <Scene key="EditUser" component={EditUserScreen} />
+            <Scene key="CreateBook" component={CreateBookScreen} />
+            <Scene key="ViewBook" component={ViewBookScreen} />
+            <Scene key="EditBook" component={EditBookScreen} />
+          </Scene>
+        </Router>
+      </Drawer>
+
     );
   }
 }
