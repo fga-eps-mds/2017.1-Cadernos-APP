fdf7f427c5b34cde295c8664005c70117a0bcfe0
Renato Valerio <renatovalerio25@gmail.com>
2017-06-01 13:52:58 -0300

    finishing create task screen

--- src/screens/create-task/create-task.component.js
+++ src/screens/create-task/create-task.component.js
@@ -1,5 +1,9 @@
 import React, { Component, PropTypes } from 'react'
-import { Container, Text, Button, Content, View } from 'native-base'
+import { Container, Text, Button, Content, View, Input, Item, Textarea } from 'native-base'
+import Navigation from '../../components/navigation-header/navigation-header.component'
+import { Dimensions } from 'react-native'
+
+const { height, width } = Dimensions.get('window');
 
 export default class CreateTask extends Component {
   static propTypes = {
@@ -8,8 +12,31 @@ export default class CreateTask extends Component {
 
   render() {
     return (
-      <Container>
-        <Text>agaraga</Text>
+      <Container style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <Navigation title='Criar Tarefa' displayGoBack={true} />
+        </View>
+        <View style={{ flex: 6, padding: 5 }}>
+          <Item>
+            <Input
+              placeholder='title'
+            />
+          </Item>
+          <Item>
+            <Textarea style={{ marginTop: 10, width, height: 200 }}
+              placeholder='Conteudo'
+            />
+          </Item>
+
+        </View>
+        <View style={{ flex: 2 }}>
+          <Button block success bordered rounded >
+            <Text> Criar </Text>
+          </Button>
+          <Button style={{ marginTop: 10 }} block warning bordered rounded >
+            <Text> Cancelar </Text>
+          </Button>
+        </View>
       </Container>
     );
   }
