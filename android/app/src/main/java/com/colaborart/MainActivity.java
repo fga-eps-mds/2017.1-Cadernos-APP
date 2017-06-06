ac1e204fbf6233990e6e06825facbfc937237194
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-06-05 21:53:27 -0300

    Fix android splash screen

--- android/app/src/main/java/com/colaborart/MainActivity.java
+++ android/app/src/main/java/com/colaborart/MainActivity.java
@@ -1,8 +1,15 @@
 package com.colaborart;
 
 import com.facebook.react.ReactActivity;
+import com.cboy.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
