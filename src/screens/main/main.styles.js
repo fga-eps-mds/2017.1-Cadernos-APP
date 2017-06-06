618efb8725847cbbcba3dfa55cfd3bb2506899a0
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-06-05 21:07:47 -0300

    Add main screean css

---
+++ src/screens/main/main.styles.js
@@ -0,0 +1,31 @@
+const styles = {
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+
+  button: {
+    width: '90%',
+    marginTop: 20,
+    borderRadius: 25,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    backgroundColor: '#FF8C2B'
+  },
+
+  title: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 35,
+    fontFamily: 'System',
+    color: '#323232'
+  },
+
+  subtitle: {
+    textAlign: 'center',
+    color: '#323232'
+  }
+}
+
+export default styles;
