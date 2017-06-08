43ed0798ce15c264e416f7fbbb00162e1441ac80
Renatovga2602 <renatovalerio25@gmail.com>
2017-06-08 13:32:32 -0300

    test task-reducer ADD task and DELETE

--- __tests__/src/reducers/task-reducer.js
+++ __tests__/src/reducers/task-reducer.js
@@ -1,8 +1,10 @@
 import { expect } from 'chai';
 
 import {
-  TASKS_SET
-} from '../../../src/config/actions-types' ;
+  TASKS_SET,
+  TASKS_DELETE,
+  TASKS_ADD
+} from '../../../src/config/actions-types';
 
 
 import taskReducer from '../../../src/reducers/tasks-reducer';
@@ -13,9 +15,38 @@ describe("Tasks reducer", () => {
 
     tasks = taskReducer(tasks, {
       type: TASKS_SET,
-      tasks: [{id: 3}, {id: 7}]
+      tasks: [{ id: 3 }, { id: 7 }]
     });
 
     expect(tasks.length).to.eq(2);
   });
+
+  it("Delete tasks", () => {
+    let tasks = [
+      { id: 3 }, { id: 7 }
+    ]
+
+    tasks = taskReducer(tasks, {
+      type: TASKS_DELETE,
+      task: { id: 7 }
+    });
+
+    expect(tasks.length).to.eq(1);
+    expect(tasks[0].id).to.eq(3);
+  });
+
+  it("Add tasks", () => {
+    let tasks = [
+      { id: 3 }
+    ]
+
+    tasks = taskReducer(tasks, {
+      type: TASKS_ADD,
+      task: { id: 7 }
+    });
+
+    expect(tasks.length).to.eq(2);
+    expect(tasks[1].id).to.eq(7);
+  });
+
 })
