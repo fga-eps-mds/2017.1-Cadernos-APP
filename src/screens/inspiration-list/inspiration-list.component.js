7c48bfd233250be52145b7c9b34e96236f451ac5
Lucas <lucas50xd@hotmail.com>
2017-06-08 20:11:22 -0300

    setting up screen for inspiration list

--- src/screens/inspiration-list/inspiration-list.component.js
+++ src/screens/inspiration-list/inspiration-list.component.js
@@ -9,7 +9,9 @@ import {
   Item,
   Input,
   List,
-  ListItem
+  ListItem,
+  Card,
+  CardItem
 } from 'native-base';
 
 import { Actions } from 'react-native-router-flux';
@@ -18,6 +20,10 @@ import NavigationHeader from '../../components/navigation-header/navigation-head
 
 
 export default class InspirationList extends Component {
+
+  componentDidMount() {
+    this.props.getInspirations(this.props.primary)
+  }
 
   handleFieldOnChange(field, value) {
     this.setState({
@@ -30,23 +36,30 @@ export default class InspirationList extends Component {
         <NavigationHeader style={{ flex: 1 }}
           title={"Inspirações"}
           displayGoBack={true} />
-        <List style={{ flex: 6 }}>
-          <ListItem>
-            <Text>Inspiração 1</Text>
-          </ListItem>
-          <ListItem>
-            <Text>Inspiração 2</Text>
-          </ListItem>
-          <ListItem>
-            <Text>Inspiração 3</Text>
-          </ListItem>
-          <ListItem>
-            <Text>Inspiração 4</Text>
-          </ListItem>
+        <List style={{flex: 6}}>
+          {this.props.inspirations.map(inspiration => {
+            return (
+              <ListItem key={inspiration.id} >
+                <Card>
+                  <CardItem>
+                    <Text>{inspiration.inspirational_title}</Text>
+                  </CardItem>
+                </Card>
+                <View style={{ flexDirection: 'column' }}>
+                  <Button danger
+                    small
+                    //onPress={() => this.props.addInspiration(this.getInspirationData(book))}
+                  >
+                    <Text>-</Text>
+                  </Button>
+                </View>
+              </ListItem>
+            );
+          })}
         </List>
 
         <Button
-        onPress={() => Actions.InspirationSearchList()}
+          onPress={() => Actions.InspirationSearchList()}
         //disabled={this.props.user.id !== this.props.book.userId}
         >
           <Text>Adicionar Inspirações</Text>
--- src/screens/inspiration-list/inspiration-list.container.js
+++ src/screens/inspiration-list/inspiration-list.container.js
@@ -1,19 +1,22 @@
 import { connect } from 'react-redux';
 import InspirationList from './inspiration-list.component';
-
+import { asyncGetInspirations } from '../../actions/inspiration-actions'
 import { Actions } from 'react-native-router-flux';
 
 
 
 const mapStateToProps = (state) => {
   return {
-
+    inspirations: state.inspirations,
+    primary: state.book
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    getInspirations(data) {
+      dispatch(asyncGetInspirations(data))
+    }
   }
 }
 
