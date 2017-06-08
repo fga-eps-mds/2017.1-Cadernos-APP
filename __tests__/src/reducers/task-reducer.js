970a1703cbeabaf90dccca0bdcf4cd6a9b065be0
Renatovga2602 <renatovalerio25@gmail.com>
2017-06-08 13:55:43 -0300

    Test Update task

--- __tests__/src/reducers/task-reducer.js
+++ __tests__/src/reducers/task-reducer.js
@@ -3,7 +3,8 @@ import { expect } from 'chai';
 import {
   TASKS_SET,
   TASKS_DELETE,
-  TASKS_ADD
+  TASKS_ADD,
+  TASKS_UPDATE
 } from '../../../src/config/actions-types';
 
 
@@ -47,6 +48,20 @@ describe("Tasks reducer", () => {
 
     expect(tasks.length).to.eq(2);
     expect(tasks[1].id).to.eq(7);
+  });
+
+   it("Update tasks", () => {
+    let tasks = [
+      { id: 3 }
+    ]
+
+    tasks = taskReducer(tasks, {
+      type: TASKS_UPDATE,
+      task: { id: 3 }
+    });
+
+    expect(tasks.length).to.eq(1);
+    expect(tasks[0].id).to.eq(3);
   });
 
 })
