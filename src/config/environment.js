1b37f187ffcbfb3bcc5a6d4614096c87c814b439
pwener <phwener@gmail.com>
2017-06-22 17:08:16 -0300

    Fixing environment production

--- src/config/environment.js
+++ src/config/environment.js
@@ -5,7 +5,7 @@ const DEVELOPMENT = {
 }
 
 const HOMOLOGATION = {
-  baseURL: 'https://cadernos-api-pwener.c9users.io/',
+  baseURL: 'https://cadernos-api.herokuapp.com',
   timeout: 5000 // 5s
 }
 
